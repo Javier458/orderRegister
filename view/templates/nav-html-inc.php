1f8b1178456c709b70f592d32d4de1063a2f35fc
Javier458 <e.javiergalvis@gmail.com>
2023-11-13 14:45:48 -0500

    add templates for optimization

---
+++ view/templates/nav-html-inc.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="https://cdn-icons-png.flaticon.com/512/2082/2082194.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="orders.php">Orders</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <a href="login.php"><button type="button" class="btn btn-outline-primary"> Login </button></a>
+          </div>
+        </div>
+      </nav>
